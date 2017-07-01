00cb8dfc8b0ea07527d24ced8633bd92bb774d9f
claudechoi <claude007@live.cn>
2017-07-01 13:06:51 +0800

    xxx

---
+++ ThirdPart-test/Expand/Category/UIView+MyFrame.h
@@ -0,0 +1,13 @@
+//
+//  UIView+MyFrame.h
+//  ThirdPart-test
+//
+//  Created by caixiaodong on 2017/6/30.
+//  Copyright © 2017年 caixiaodong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (MyFrame)
+
+@end
---
+++ ThirdPart-test/Expand/Category/UIView+MyFrame.m
@@ -0,0 +1,13 @@
+//
+//  UIView+MyFrame.m
+//  ThirdPart-test
+//
+//  Created by caixiaodong on 2017/6/30.
+//  Copyright © 2017年 caixiaodong. All rights reserved.
+//
+
+#import "UIView+MyFrame.h"
+
+@implementation UIView (MyFrame)
+
+@end
