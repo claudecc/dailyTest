ec889733ed7aba9223b6c59d811e9bce8ec63a79
claudechoi <claude007@live.cn>
2017-10-25 13:53:17 +0800

    hidden

--- ThirdPart-test/Classes/Circle/Controller/PersonVC.m
+++ ThirdPart-test/Classes/Circle/Controller/PersonVC.m
@@ -17,6 +17,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.navigationController.navigationBarHidden = YES;
+    
+    [self setupUI];
 }
 
 - (void)didReceiveMemoryWarning {
